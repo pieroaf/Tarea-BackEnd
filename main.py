140269d1bc206c585e543854a08ee565412c15e3
Piero Azahuanche <pieroazahuanchef@gmail.com>
2022-11-06 22:38:08 -0500

    añadiendo clase libro

--- main.py
+++ main.py
@@ -1,37 +1,33 @@
+import pickle
+
 libros = []
 
-while True:
-    opcion = int(input('que opcion: '))
-    if opcion == 1:
-        print('falta')
-    if opcion == 2:
-        for cont, i in enumerate(libros):
-            print(f'{cont} - {i}')
-    if opcion == 3:
-        id = int(input('id: '))
-        titulo = input('titulo: ')
-        autores = []
-        numero_autores = int(input('numero de autores: '))
-        for i in range(1, numero_autores + 1):
-            autor = input(f'autor {i}: ')
-            autores.append(autor)
-        libro = {}
-        libro['id'] = id
-        libro['titulo'] = titulo
-        libro['autores'] = autores
-        libros.append(libro)
-    if opcion == 4:
-        titulo = input("eliminar titulo: ")
-        for e in libros:
-            if e["titulo"] == titulo:
-                libros.remove(e)
-    if opcion == 5:
-        titulo = input("buscar titulo: ")
-        for e in libros:
-            if e["titulo"] == titulo:
-                print(libros)
-    if opcion == 6:
-        print('falta')
+class mostrar_opciones():
+    def Opciones(self):
+        print('================================LISTA DE OPCIONES======================')
+        print('Opción 1: Leer archivo de disco duro (.txt o csv).')
+        print('Opción 2: Listar libros.')
+        print('Opción 3: Agregar libro.')
+        print('Opción 4: Eliminar libro.')
+        print('Opción 5: Buscar libro por ISBN o por título.')
+        print('Opción 6: Ordenar libros por título.')
+        print('Opción 7: Buscar libros por autor, editorial o género.')
+        print('Opción 8: Buscar libros por número de autores.')
+        print('Opción 9: Editar o actualizar datos de un libro.')
+        print('Opción 10: Guardar libros en archivo de disco duro (.txt o csv).')
+        print('Opción 11: Salir :)')
+        print('=======================================================================')
+
+class leer_libro():
+    def leer(self):
+        with open("libros.txt", "rb") as f:
+            agregar = pickle.load(f)
+            for i in agregar:
+                libros.append(i)
+            print('======================LISTANDO LIBROS AGREGADOS=======================')
+            for cont, i in enumerate(libros):
+                print(f'{cont} - {i}')
+            print('=======================================================================')
 
 
 
