5b7d315f374c51b76aa9ae94fd0a7c981f3d0144
Piero Azahuanche <pieroazahuanchef@gmail.com>
2022-11-06 22:54:38 -0500

    Añadiendo 3 clases más

--- main.py
+++ main.py
@@ -30,5 +30,57 @@ class leer_libro():
             print('=======================================================================')
 
 
+class mostrar_libros():
+    def mostrar(self, fi):
+        self.f = fi
+        print('===========================LISTANDO LIBROS=============================')
+        for cont, i in enumerate(self.f):
+            print(f'{cont} - {i}')
+        print('=======================================================================')
+
+class agregar_libros():
+    def agregar(self, id, titulo, genero, isbn, editorial, autores):
+        self.id = id
+        self.titulo = titulo
+        self.genero = genero
+        self.isbn = isbn
+        self.editorial = editorial
+        self.autores = autores
+        libro = {}
+        libro['id'] = self.id
+        libro['titulo'] = self.titulo
+        libro['genero'] = self.genero
+        libro['isbn'] = self.isbn
+        libro['editorial'] = self.editorial
+        libro['autores'] = self.autores
+        libros.append(libro)
+        print('=================================AGREGADO==============================')
+
+class eliminar_libros():
+    def eliminar(self, titulo):
+        self.titulo = titulo
+        for e in libros:
+            if e["titulo"] == self.titulo:
+                libros.remove(e)
+        print('================================ELIMINADO=============================')
+
+class buscar_libros():
+    def buscar(self, valor):
+        self.valor = valor
+        print('========================RESULTADO DE BUSQUEDA==========================')
+        for e in libros:
+            if e["titulo"] == self.valor or e["isbn"] == self.valor:
+                print(e)
+        print('=======================================================================')
+
+class ordenar_libros():
+    def ordenar(self):
+        ordenado = sorted(libros, key=lambda k: k['titulo'])
+        print('========================LISTA ORDENADA==========================')
+        for cont, i in enumerate(ordenado):
+            print(f'{cont} - {i}')
+        print('=======================================================================')
+
+
 
 
